8d7b6d6e2f78bb52debc0541c1f7415276afd895
phalcobtw <laxz_16@hotmail.com>
2022-09-01 21:23:13 -0600

    bloc finalizado

--- test/clases_test.dart
+++ test/clases_test.dart
@@ -1,11 +1,12 @@
+import 'package:flutter_application_1/clases.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:bloc_test/bloc_test.dart';
 
 void main() {
-  blocTest<SubjectBloc, SubjectState>(
+  blocTest<ClaseBloc, EstadoVerificacion>(
     'emits [MyState] when MyEvent is added.',
-    build: () => SubjectBloc(),
-    act: (bloc) => bloc.add(MyEvent),
-    expect: () => const <SubjectState>[MyState],
+    build: () => ClaseBloc(),
+    act: (bloc) => bloc.add(Creo()),
+    expect: () => const [isA<SolicitandoNombre>],
   );
 }
