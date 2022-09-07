881c5bb157d3407d0424a67a688aa280a2f47d54
phalcobtw <laxz_16@hotmail.com>
2022-09-07 17:18:51 -0600

    cree el STF widget para actualiar el button

--- lib/vista_esperando_nombre.dart
+++ lib/vista_esperando_nombre.dart
@@ -16,3 +16,45 @@ class VistaSolicitandoNombre extends StatelessWidget {
     );
   }
 }
+
+class VistaSolicitandoNombreSTF extends StatefulWidget {
+  const VistaSolicitandoNombreSTF({super.key});
+
+  @override
+  State<VistaSolicitandoNombreSTF> createState() => _VistaSolicitandoNombreSTFState();
+}
+
+class _VistaSolicitandoNombreSTFState extends State<VistaSolicitandoNombreSTF> {
+  bool isButtonActive = true;
+  late TextEditingController controller;
+  @override
+  void initState(){
+    super.initState();
+    controller = TextEditingController();
+    controller.addListener(() {
+      final isButtonActive = controller.text.isNotEmpty;
+
+      setState(() => this.isButtonActive = isButtonActive);
+    });
+  }
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Dame el nombre'),
+        TextField(
+          controller: controller,
+        ),
+        TextButton(onPressed: isButtonActive ? () {
+          setState(() => isButtonActive = false,);
+          controller.clear();
+        }:null, child: Text('Algo debe decir aqui'))
+      ],
+    );
+  }
+}
