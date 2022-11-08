563c33acc996939759293053b1f6efad42e70084
phalcobtw <laxz_16@hotmail.com>
2022-11-08 00:27:37 -0700

    vista mostrnado la lista de juegos desde txt

---
+++ lib/vista_listajuegos.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/clases.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'dominio/juego_jugado.dart';
+
+
+
+
+class VistaListaJuegos extends StatelessWidget {
+  const VistaListaJuegos(
+      {Key? key, required this.setJuegos, required this.nombre})
+      : super(key: key);
+  final String nombre;
+  final Set<JuegoJugado> setJuegos;
+  @override
+  Widget build(BuildContext context) {
+    List<JuegoJugado> listaJuegos = setJuegos.toList();
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: setJuegos.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: Text((index + 1).toString()),
+                subtitle: Text(listaJuegos[index].id.toString()),
+                title: Text(listaJuegos[index].nombre.toString()),
+              );
+            },
+          ),
+        ),
+        FloatingActionButton(
+          onPressed: () {
+            var bloc = context.read<ClaseBloc>();
+            bloc.add(NombreRecibido(nombre));
+          },
+          child: const Text('Regresar'),
+        )
+      ],
+    );
+  }
+}
