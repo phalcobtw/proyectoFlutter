bc1e333d361926a5a6f92c3612ad1d900ce459af
phalcobtw <laxz_16@hotmail.com>
2022-11-07 22:16:34 -0700

    test X juego y num paginas api online

--- test/caracteristicas/verificacion/repositorio_xml_test.dart
+++ test/caracteristicas/verificacion/repositorio_xml_test.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_application_1/caracteristicas/verificacion/repositorio_xml.dart';
+import 'package:flutter_application_1/caracteristicas/verificacion/vistas/repositorio_juegos_verificacion.dart';
+import 'package:flutter_application_1/dominio/juego_jugado.dart';
 import 'package:flutter_application_1/dominio/nick_formado.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -33,5 +35,19 @@ test('benthor online', () async {
     resultado.match((l) {
       expect(true, equals(false));
     }, (r) => expect(r.length, equals(18)));
+});
+
+test('benthor tiene takenoko en jugados', () async{
+  RepositorioXmlReal repositorioXml = RepositorioXmlReal();
+  RepositorioPruebasJuegosJugados repositorioJuegos = RepositorioPruebasJuegosJugados(repositorioXml);
+  final takenoko = JuegoJugado.constructor(nombrePropuesto: 'Takenoko', idPropuesto: '70919');
+  final respuesta = await repositorioJuegos.obtenerJuegosJugados(NickFormado.constructor('benthor'));
+      respuesta.match(
+        (l){
+          assert(false);
+        }, 
+        (r){
+          expect(r.contains(takenoko), true);
+        });
 });
 }
